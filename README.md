aa2e911542ac384e5e1045c20e322b50259fe728
Dairyo <sekinedairyou@gmail.com>
2019-11-23 12:22:45 +0900

    aaa

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
+# concept
+this app enables you to enjoy theme parks such as disney resort, USJ and so on!
+
+# prepare
+before you use this app, you have to invite this app by QR code or ID.
+
+ID: `@541rhynx`
+
+QR code:
+
 # usage
+you can use the functions below.
+
+don't forget leading `:`. (e.g.`:help`)
+
+myapp ignores messages without `:`.
+
+## help
+show the link to this page.
+
+
+## comb, combination
+
+## expense
+### expence *integer*
+### expence clear
+### expence split
+
+## bye
+kick out this app from the group or talk room.
+
+when you are talkig alone, nothing will happen.
+<!--
+## birthday
+-->
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# from https://github.com/line/line-bot-sdk-python
 try:
   import googleclouddebugger # debugger for google app engine
   googleclouddebugger.enable()
@@ -6,40 +7,83 @@ except ImportError:
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 import config
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(config.token)
 handler = WebhookHandler(config.secret)
+url_github = "https://github.com/dr666m1/project_theme_park_bot"
 
 @app.route("/")
 def github():
-    return redirect("https://github.com/sekinedairyou/project_theme_park_bot")
+    return redirect(url_github)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-   # get X-Line-Signature header value
-   signature = request.headers['X-Line-Signature']
-   # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body) # output log to stdout
-   # handle webhook body
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       print("Invalid signature. Please check your channel access token/channel secret.")
-       abort(400)
-   return 'OK'
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body) # output log to stdout
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+    return 'OK'
+
+def error(event):
+    msg = 'sorry, something went wrong. if you need help, type ":help" and check the usage.'
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=msg)
+    )
+
+def help(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=url_github)
+    )
+
+def bye(event):
+    try:
+        line_bot_api.leave_group(event.source.group_id)
+    except AttributeError as e:
+        pass
+    try:
+        line_bot_api.leave_room(event.source.source_id)
+    except AttributeError as e:
+        pass
+
+def birthday(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        [
+            TextSendMessage(text="なんと..."),
+            TextSendMessage(text="今日はゆっこちゃんに..."),
+            TextSendMessage(text="誕生日プレゼントがあります！！"),
+            #ImageSendMessage(
+            #    original_content_url="",
+            #    preview_image_url="",
+            #),
+        ]
+    )
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   line_bot_api.reply_message(
-       event.reply_token,
-       TextSendMessage(text=event.message.text))
-
+    cmd = event.message.text.split()
+    if cmd[0] == ":help":
+        help(event)
+    elif cmd[0] == ":bye":
+        bye(event)
+    elif cmd[0] == ":birthday":
+        birthday(event)
+    elif cmd[0][0] == ":":
+        error(event)
 
 if __name__ == "__main__":
-   app.run()
+    app.run()
 
