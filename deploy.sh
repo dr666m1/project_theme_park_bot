815c743e1ef814c413b12cd47dd4477c6357c32a
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-04-11 16:16:30 +0900

    routine

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
-gcloud app deploy
+gcloud builds submit --tag gcr.io/$gcp_project/qiita-sample
+gcloud beta run deploy qiita-sample --image gcr.io/$gcp_project/qiita-sample --platform managed --region us-west1
